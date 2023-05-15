16fd1bb51e991ba744af194babef948afbbcd6fe
Trevor Steil <trevor.steil1@gmail.com>
2023-05-15 13:22:48 -0700

    Adding print statements to disjoint set compress

--- include/ygm/container/detail/disjoint_set_impl.hpp
+++ include/ygm/container/detail/disjoint_set_impl.hpp
@@ -415,11 +415,11 @@ class disjoint_set_impl {
 
       m_comm.barrier();
 
-      world.cout0("Total items: ", size());
-      world.cout0("Max rank items: ",
-                  m_comm.all_reduce_max(m_local_item_parent_map.size()));
-      world.cout0("Min rank items: ",
-                  m_comm.all_reduce_min(m_local_item_parent_map.size()));
+      m_comm.cout0("Total items: ", size());
+      m_comm.cout0("Max rank items: ",
+                   m_comm.all_reduce_max(m_local_item_parent_map.size()));
+      m_comm.cout0("Min rank items: ",
+                   m_comm.all_reduce_min(m_local_item_parent_map.size()));
     }
   }
 
