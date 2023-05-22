107376d12ef13cab9d2c4c2b4cc03938c0063c1a
Trevor Steil <trevor.steil1@gmail.com>
2023-05-22 10:50:37 -0700

    Removing print statements from debugging

--- include/ygm/container/detail/disjoint_set_impl.hpp
+++ include/ygm/container/detail/disjoint_set_impl.hpp
@@ -238,9 +238,6 @@ class disjoint_set_impl {
     static auto update_parent_and_cache_lambda =
         [](auto p_dset, auto &item_info, const value_type &old_parent,
            const value_type &new_parent, const rank_type &new_rank) {
-          // p_dset->m_comm.cout()
-          //<< "Setting cache for " << old_parent << ": (" << new_rank << ", "
-          //<< new_parent << ")" << std::endl;
           p_dset->m_cache.add_cache_entry(old_parent,
                                           rank_parent_t(new_rank, new_parent));
 
@@ -472,11 +469,6 @@ class disjoint_set_impl {
 
       m_comm.barrier();
     }
-    m_comm.cout0("Total items: ", size());
-    m_comm.cout0("Max rank items: ",
-                 m_comm.all_reduce_max(m_local_item_parent_map.size()));
-    m_comm.cout0("Min rank items: ",
-                 m_comm.all_reduce_min(m_local_item_parent_map.size()));
   }
 
   template <typename Function>
