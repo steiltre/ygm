b73eaad3197be13abd9ad232342979f6a944d320
Trevor Steil <trevor.steil1@gmail.com>
2025-08-03 19:27:20 -0700

    Adds missing locale header to line_parser.hpp

--- include/ygm/io/line_parser.hpp
+++ include/ygm/io/line_parser.hpp
@@ -7,6 +7,7 @@
 
 #include <filesystem>
 #include <fstream>
+#include <locale>
 #include <string>
 #include <vector>
 #include <ygm/container/detail/base_iteration.hpp>
@@ -183,7 +184,7 @@ class line_parser : public ygm::container::detail::base_iteration_value<
       size_t bytes_begin = std::get<1>(fname);
       size_t bytes_end   = std::get<2>(fname);
       YGM_ASSERT_RELEASE(ifs.good());
-      // ifs.imbue(std::locale::classic());
+      ifs.imbue(std::locale::classic());
       StringType line;
       bool       first_line = false;
       // Throw away line containing bytes_begin as it was read by the previous
