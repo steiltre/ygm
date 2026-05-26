36cde9b571836c178583945154f98dc2456f26f4
Trevor Steil <trevor.steil1@gmail.com>
2026-05-25 20:02:05 -0700

    Adds missing braces to test_subcommunicators to destroy ygm::comm before calling MPI_Finalize()

--- test/test_subcommunicators.cpp
+++ test/test_subcommunicators.cpp
@@ -10,44 +10,46 @@
 int main() {
   YGM_ASSERT_MPI(MPI_Init(nullptr, nullptr));
 
-  ygm::comm world(MPI_COMM_WORLD);
-
-  //
-  // Test initializing YGM communicator from subcommunicator
   {
-    MPI_Comm self_mpi_comm;
-    MPI_Comm_split(MPI_COMM_WORLD, world.rank(), 0, &self_mpi_comm);
+    ygm::comm world(MPI_COMM_WORLD);
 
-    ygm::comm self_comm(self_mpi_comm);
-
-    YGM_ASSERT_RELEASE(self_comm.size() == 1);
-    YGM_ASSERT_RELEASE(self_comm.rank() == 0);
-
-    bool flag     = false;
-    auto flag_ptr = self_comm.make_ygm_ptr(flag);
-
-    self_comm.async(0, [](auto flag_ptr) { *flag_ptr = true; }, flag_ptr);
-
-    self_comm.barrier();
-    YGM_ASSERT_RELEASE(flag == true);
-  }
-
-  //
-  // Test container creation on subcommunicators
-  {
+    //
+    // Test initializing YGM communicator from subcommunicator
     {
       MPI_Comm self_mpi_comm;
       MPI_Comm_split(MPI_COMM_WORLD, world.rank(), 0, &self_mpi_comm);
 
       ygm::comm self_comm(self_mpi_comm);
 
-      for (int i = 0; i < world.rank(); ++i) {
-        ygm::container::bag<int> b(self_comm);
-      }
+      YGM_ASSERT_RELEASE(self_comm.size() == 1);
+      YGM_ASSERT_RELEASE(self_comm.rank() == 0);
+
+      bool flag     = false;
+      auto flag_ptr = self_comm.make_ygm_ptr(flag);
+
+      self_comm.async(0, [](auto flag_ptr) { *flag_ptr = true; }, flag_ptr);
+
+      self_comm.barrier();
+      YGM_ASSERT_RELEASE(flag == true);
     }
 
-    // Return to world
-    ygm::container::bag<int> b(world);
+    //
+    // Test container creation on subcommunicators
+    {
+      {
+        MPI_Comm self_mpi_comm;
+        MPI_Comm_split(MPI_COMM_WORLD, world.rank(), 0, &self_mpi_comm);
+
+        ygm::comm self_comm(self_mpi_comm);
+
+        for (int i = 0; i < world.rank(); ++i) {
+          ygm::container::bag<int> b(self_comm);
+        }
+      }
+
+      // Return to world
+      ygm::container::bag<int> b(world);
+    }
   }
 
   MPI_Finalize();
